11ac8a22173799c21398ed0e7f4d570167591b35
SeungJuChoi <csjaj9772@gmail.com>
2016-08-05 03:37:03 +0900

    AC with math

--- CodeForces/div.2/365/B.cpp
+++ CodeForces/div.2/365/B.cpp
@@ -33,3 +33,58 @@ public:
 		}return ret;
 	}
 };
+
+int main() {
+
+	int n, k;
+	scanf("%d %d", &n, &k);
+
+	for (int i = 1; i <= n; i++) {
+		scanf("%lld", &c[i]);
+	}
+
+	c[n + 1] = c[1];
+
+	BIT tree(n);
+
+	for (int i = 1; i <= k; i++) {
+
+		int x;
+		scanf("%d", &x);
+
+		tree.Update(x, c[x]);
+
+		s[x] = true;
+
+	}
+
+	ll ans = 0;
+
+	ll se = 0, xy = 0;
+
+	for (int i = 1; i <= n; i++) {
+		
+		int nxt = i + 1;
+
+		if (nxt > n)nxt = 1;
+
+		if (i <= n && (s[i] || s[nxt])) {
+			
+		}
+		else {
+			ans += c[i] * c[i + 1];
+		}
+
+		if (s[i]) {
+			ans += (tree.Sum(n) - tree.Sum(i)) * c[i];
+			se += c[i];
+		}
+		else {
+			xy += c[i];
+		}
+
+	}
+
+	printf("%lld\n", ans + se * xy);
+
+}

04d2c1c71938c597c6c1681e081e4676797b30b1
SeungJuChoi <csjaj9772@gmail.com>
2016-08-07 01:27:38 +0900

    AC with implementation(parametric search)

--- BOJ/2343/solution.cpp
+++ BOJ/2343/solution.cpp
@@ -1,52 +1,53 @@
 #include <stdio.h>
+#include <limits.h>
+#include <algorithm>
+
+using namespace std;
 
 int b[100003];
 
-int main(){
+int main() {
 	int n, k;
 	scanf("%d %d", &n, &k);
 
-	for (int i = 1; i <= n; i++){
+	for (int i = 1; i <= n; i++) {
 		scanf("%d", &b[i]);
 	}
 
 	int l = 1, r = 1000000000, m;
 
-	while (l <= r){
+	int ans = INT_MAX;
+
+	while (l <= r) {
+
 		m = (l + r) / 2;
 
 		int w = 0, c = 1;
-		for (int i = 1; i <= n; i++){
-			if (w + b[i] <= m){
+		for (int i = 1; i <= n; i++) {
+			if (w + b[i] <= m) {
 				w += b[i];
 			}
-			else{
-				c++;
-				w = b[i];
+			else {
+				if (m < b[i]) {
+					c = k + 3;
+					break;
+				}
+				else {
+					w = b[i];
+					c++;
+				}
 			}
 		}
 
-		if (c > k){
+		if (c > k) {
 			l = m + 1;
 		}
-		else
-			r = m - 1;
-	}
-
-	int w = 0, c = 1;
-	for (int i = 1; i <= n; i++){
-		if (w + b[i] <= m){
-			w += b[i];
-		}
 		else{
-			c++;
-			w = b[i];
+			ans = min(ans, m);
+			r = m - 1;
 		}
 	}
 
-	if (c > k){
-		m++;
-	}
 
-	printf("%d\n", m);
+	printf("%d\n", ans);
 }

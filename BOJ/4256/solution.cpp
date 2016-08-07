a071c35aa7503f880e6f70748b36ec040de18d55
SeungJuChoi <csjaj9772@gmail.com>
2016-08-08 03:20:58 +0900

    AC with implementation(recursive function)

--- BOJ/4256/solution.cpp
+++ BOJ/4256/solution.cpp
@@ -1,24 +1,23 @@
 #include <stdio.h>
-#include <string.h>
-#include <vector>
-#include <algorithm>
 
-using namespace std;
+int pre[1003], in[1003];
 
-vector <int> slice(const vector <int> &v, int a, int b) {
-	return vector <int>(v.begin() + a, v.begin() + b);
-}
+void split(int pl, int pr, int il, int ir) {
 
-void Consist(const vector <int> &pre, const vector <int> &in) {
-	int S = pre.size();
-	if (!S)return;
-	char root = pre[0];
+	if (pr - pl == 0)return;
 
-	int L = find(in.begin(), in.end(), root) - in.begin();
-	int R = S - 1 - L;
-	Consist(slice(pre, 1, L + 1), slice(in, 0, L));
-	Consist(slice(pre, L + 1, S), slice(in, L + 1, S));
+	int root = pre[pl];
+
+	for (int i = il; i < ir; i++) {
+		if (in[i] == root) {
+			split(pl + 1, pl + 1 + (i - il), il, i);
+			split(pl + 1 + (i - il), pr, i + 1, ir);
+			break;
+		}
+	}
+
 	printf("%d ", root);
+
 }
 
 int main()
@@ -32,13 +31,13 @@ int main()
 		int n;
 		scanf("%d", &n);
 
-		vector <int> pre(n), in(n);
-
 		for (int i = 0; i < n; i++)scanf("%d", &pre[i]);
 		for (int i = 0; i < n; i++)scanf("%d", &in[i]);
 
-		Consist(pre, in);
+		split(0, n, 0, n);
+
 		printf("\n");
+
 	}
 
 }

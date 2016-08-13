50d60004ac51f8e010541ee29ea115757de23f41
SeungJuChoi <csjaj9772@gmail.com>
2016-08-14 01:51:39 +0900

    AC with implementation(parametric search)

--- BOJ/2243/solution.cpp
+++ BOJ/2243/solution.cpp
@@ -1,39 +1,87 @@
+#include <vector>
 #include <stdio.h>
-#include <set>
-#include <map>
 #include <algorithm>
 
 using namespace std;
 
-int main(){
+class BIT {
+public:
 
-	int n;
-	scanf("%d", &n);
+	vector <int> T;
+	int S;
 
-	set <int> candy;
-	map <int, int> cnt;
+	BIT(const int n) {
+		S = n;
+		T.resize(S + 1);
+	}
 
-	for (int i = 1; i <= n; i++){
-		int a, b;
-		scanf("%d %d", &a, &b);
+	void Update(int p, int v) {
+		while (p <= S) {
+			T[p] += v;
+			p += p & (-p);
+		}
+	}
 
-		if (a == 1){
-			// pick up
+	long long Sum(int p) {
+		long long ret = 0;
+		if (p == 0)return 0;
+		while (p > 0) {
+			ret += T[p];
+			p -= p & (-p);
+		}return ret;
+	}
 
-			set <int> ::iterator it = candy.begin();
+};
+
+int main() {
+
+
+	BIT tree(1000003);
+
+	int q;
+	scanf("%d", &q);
+
+	while(q--){
+
+		int t, x;
+		scanf("%d %d", &t, &x);
+
+		if (t == 2) {
+
+			int a;
+			scanf("%d", &a);
+
+			tree.Update(x, a);
 
 		}
-		else{
-			// insert
-			int c;
-			scanf("%d", &c);
+		else {
 
-			if (cnt[b] == 0)candy.insert(b);
+			// x-th what
 
-			cnt[b] += c;
+			int l = 1, r = 1000000, m;
 
-			if (cnt[b] == 0)candy.erase(b);
+			while (l <= r) {
+
+				m = (l + r) / 2;
+
+				if (tree.Sum(m - 1) < x && x <= tree.Sum(m)) {
+
+					printf("%d\n", m);
+
+					tree.Update(m, -1);
+
+					break;
+
+				}
+
+				if (tree.Sum(m) >= x)r = m - 1;
+				else
+					l = m + 1;
+
+			}
+
 		}
+
 	}
 
 }

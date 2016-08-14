889de8e9f68c4a6c8199953fac648918d2b2e7ed
SeungJuChoi <csjaj9772@gmail.com>
2016-08-14 10:28:20 +0900

    add kcm1700's ext gcd and mod inversion

---
+++ MYLIBRARY/math.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <stdio.h>
+
+
+/* find a pair (c,d) s.t. ac + bd = gcd(a,b)
+* Dependencies: none */
+pair<long long, long long> extended_gcd(long long a, long long b) {
+	if (b == 0) return make_pair(1, 0);
+	pair<long long, long long> t = extended_gcd(b, a % b);
+	return make_pair(t.second, t.first - t.second * (a / b));
+}
+
+/* Find x in [0,m) s.t. ax ≡ gcd(a, m) (mod m)
+* Dependencies: extended_gcd(a, b) */
+long long modinverse(long long a, long long m) {
+	return (extended_gcd(a, m).first % m + m) % m;
+}

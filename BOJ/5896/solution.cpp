e353e793f9680cf9947a17e52d07a3a0fc02b284
SeungJuChoi <csjaj9772@gmail.com>
2016-08-15 18:43:09 +0900

    AC with greedy

--- BOJ/5896/solution.cpp
+++ BOJ/5896/solution.cpp
@@ -1,153 +1,80 @@
-//#include <stdio.h>
-//#include <queue>
-//#include <vector>
-//#include <algorithm>
-//
-//#define ll long long 
-//
-//using namespace std;
-//
-//class ele{
-//public:
-//
-//	int mV, flg;
-//	int pay, cpay;
-//
-//	ele(){}
-//	ele(int a, int b, int c, int d){ mV = a, flg = b, pay = c, cpay = d; }
-//
-//	bool operator<(const ele &A)const{
-//		if (mV == A.mV)return flg < A.flg;
-//		return mV > A.mV;
-//	}
-//
-//};
-//
-//int main(){
-//
-//	int n, k;
-//	ll m;
-//	scanf("%d %d %lld", &n, &k, &m);
-//
-//	priority_queue <ele> q;
-//	for (int i = 1; i <= n; i++){
-//		int a, b;
-//		scanf("%d %d", &a, &b);
-//
-//		if (a > b)q.emplace(b, 1, a, b);
-//		else
-//			q.emplace(a, 0, a, b);
-//	}
-//
-//	int ans = 0;
-//	while (!q.empty() && m){
-//		ele f = q.top(); q.pop();
-//		
-//		if (f.mV > m)break;
-//		else{
-//			if (f.flg){
-//				if (k){
-//					m -= f.mV;
-//					ans++;
-//					k--;
-//				}
-//				else{
-//					q.emplace(f.pay, 0, f.pay, f.cpay);
-//				}
-//			}
-//			else{
-//				m -= f.mV;
-//				ans++;
-//			}
-//		}
-//	}
-//
-//	printf("%d\n", ans);
-//
-//}
-
 #include <stdio.h>
-#include <queue>
-#include <vector>
-#include <algorithm>
 
-#define ll long long 
+#include <vector>
+
+#include <queue>
+
+#include <functional>
 
 using namespace std;
 
-class ele{
-
-public:
-
-	int v, ind;
-
-	ele(){}
-	ele(int a, int b){ v = a, ind = b; }
-
-	bool operator<(const ele &A)const{
-		return v  A.v;
-	}
-
-};
+int pay[50003][2];
 
 bool sold[50003];
 
-int main(){
+#define ll long long
+#define cost first
+#define id second
+
+int main() {
 
 	int n, k;
 	ll m;
 	scanf("%d %d %lld", &n, &k, &m);
 
-	priority_queue <ele> ch, ex;
-	for (int i = 1; i <= n; i++){
-		int a, b;
-		scanf("%d %d", &a, &b);
+	for (int i = 1; i <= n; i++) {
 
-		ex.emplace(a, i);
-		ch.emplace(b, i);
-	}
-
-	int ans = 0;
-
-	while (m && ans < n){
-
-		while (!ex.empty()){
-			if (sold[ex.top().ind])ex.pop();
-			else
-				break;
-		}
-
-		while (!ch.empty()){
-			if (sold[ch.top().ind])ch.pop();
-			else
-				break;
-		}
-
-		if (k && ch.top().v < ex.top().v){ // wrong answer ¤Ð¤Ð
-			if (ch.top().v > m)break;
-			sold[ch.top().ind] = true;
-			m -= ch.top().v;
-			ans++;
-			k--;
-		}
-		else{
-			if (ex.top().v > m)break;
-			sold[ex.top().ind] = true;
-			m -= ex.top().v;
-			ans++;
-		}
+		scanf("%d %d", &pay[i][1], &pay[i][0]);
 
 	}
 
-	printf("%d\n", ans);
+	priority_queue < int, vector <int>, greater<int> > res;
+
+	priority_queue < pair <int, int>, vector < pair <int, int> >, greater < pair<int,int> > > low, high;
+
+	for (int i = 1; i <= n; i++) {
+		low.emplace(pay[i][0], i);
+		high.emplace(pay[i][1], i);
+	}
+
+	for (int i = 1; i <= k; i++)res.push(0);
+
+	int bought = 0;
+
+	while (m > 0 && bought < n) {
+
+		while (!low.empty() && sold[low.top().second])low.pop();
+		while (!high.empty() && sold[high.top().second])high.pop();
+
+		ll cst = res.top() + low.top().cost;
+
+		if (cst < high.top().cost) {
+
+			if (m < cst)break;
+
+			m -= cst;
+
+			// first use coupon, but at last do not use coupon
+			res.pop();
+			res.push(pay[low.top().id][1] - pay[low.top().id][0]);
+
+			sold[low.top().id] = true;
+			low.pop();
+
+		}
+		else {
+
+			if (m < high.top().cost)break;
+
+			m -= high.top().cost;
+			sold[high.top().id] = true;
+
+		}
+
+		bought++;
+
+	}
+
+	printf("%d\n", bought);
 
 }
-
-/*
-5 1 9
-2 1
-8 1
-4 3
-5 4
-6 3
-*/

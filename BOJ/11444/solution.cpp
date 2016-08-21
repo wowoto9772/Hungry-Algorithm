b14f00f1ea6eab9f9b80546351189772d8611cc2
SeungJuChoi <csjaj9772@gmail.com>
2016-08-21 13:47:50 +0900

    AC with math

---
+++ BOJ/11444/solution.cpp
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <map>
+
+#define ll long long
+#define mod 1000000007
+
+using namespace std;
+
+map <ll, ll> F;
+
+ll fibonacci(ll a)
+{
+	if (a < 2) return 1;
+	if (F[a]) return F[a];
+	return F[a] = (fibonacci((a + 1) / 2)*fibonacci(a / 2) + fibonacci((a - 1) / 2)*fibonacci((a - 2) / 2)) % mod;
+}
+
+int main()
+{
+	ll a;
+	scanf("%lld", &a);
+
+	printf("%lld\n", (a == 0 ? 0 : fibonacci(a - 1)));
+}

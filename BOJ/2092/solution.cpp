bb4b95e5df8915508e4153653dbe5d6abb32e5de
sexy_wowoto <csjaj9772@gmail.com>
2016-08-27 15:45:10 +0900

    AC with dp

--- BOJ/2092/solution.cpp
+++ BOJ/2092/solution.cpp
@@ -1,13 +1,58 @@
 #include <stdio.h>
+#include <algorithm>
 
-int main(){
+using namespace std;
+
+#define mod 1000000
+
+int dp[4004][204];
+int c[204];
+
+int dy(int m, int v){
+
+	int &ret = dp[m][v];
+
+	if(m == 0)return 1;
+	else if(v == 0)return 0;
+
+	if(ret != -1)return ret;
+
+	ret = 0;
+
+	ret += dy(m, v-1);
+
+	for(int i=1; i<=min(m, c[v]); i++){
+		ret += dy(m-i, v-1);
+		ret %= mod;
+	}
+
+	return ret;
 
 }
 
-/*
-K=1 : {1}, {2}, {3}
-K=2 : {1, 1}, {1, 2}, {1, 3}, {2, 2}, {2, 3}
-K=3 : {1, 1, 2} {1, 1, 3} {1, 2, 2} {1, 2, 3} {2, 2, 3}
-K=4 : {1, 2, 2, 3} {1, 1, 2, 2} {1, 1, 2, 3}
-K=5 : {1, 1, 2, 2, 3}
-*/
+int main(){
+
+	int T, A, S, B;
+	scanf("%d %d %d %d", &T, &A, &S, &B);
+
+	for(int i=1; i<=A; i++){
+		int v;
+		scanf("%d", &v);
+
+		c[v]++;
+	}
+
+	for(int i=1; i<=T; i++){
+		for(int j=1; j<=A; j++)dp[j][i] = -1;
+	}
+
+	int ans = 0;
+
+	for(int i=min(S,B); i<=max(S,B); i++){
+		ans += dy(i, T);
+		ans %= mod;
+	}
+
+	printf("%d\n", ans);
+
+}

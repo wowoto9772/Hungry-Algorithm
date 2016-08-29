1cccaac8b1f7fb8f12d3d06b23c5093fa63bf18a
sexy_wowoto <csjaj9772@gmail.com>
2016-08-29 19:06:23 +0900

    added tools auto compile, tistory source maker

---
+++ BOJ/tistory_solution_maker.py
@@ -0,0 +1,29 @@
+src = open("./solution.cpp", "r");
+dat = open("./tistory.out", "w");
+
+header = '<pre class = "line-numbers"><code class = "language-cpp"'
+footer = '</code></pre>'
+
+dat.writelines(header)
+
+while True:
+	linedata = src.readline()
+
+	if not linedata: break
+
+	out_line = ""
+
+	for c in linedata:
+		if c == '<':
+			out_line += "&lt;"	
+		elif c == '>':
+			out_line += "&gt;"
+		else:
+			out_line += c;
+
+	dat.writelines(out_line)
+
+dat.writelines(footer)
+
+src.close()
+dat.close()

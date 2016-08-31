e3caaabe2a2bedc0886c6439d18b59bffa08100d
sexy_wowoto <csjaj9772@gmail.com>
2016-09-01 01:39:06 +0900

    AC with floyd warshall

---
+++ BOJ/6462/solution.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <limits.h>
+#include <algorithm>
+#define n 20
+
+using namespace std;
+
+int d[22][22];
+
+int main(){
+
+	int f;
+	int tc = 0;
+	
+	while (scanf("%d", &f) == 1){
+		for (int i = 1; i <= 20; i++){
+			for (int j = 1; j <= 20; j++){
+				d[i][j] = INT_MAX;
+			}
+		}
+		for (int i = 1; i <= f; i++){
+			int a;
+			scanf("%d", &a);
+			d[1][a] = d[a][1] = 1;
+		}
+		for (int i = 2; i < n; i++){
+			scanf("%d", &f);
+			for (int j = 1; j <= f; j++){
+				int a;
+				scanf("%d", &a);
+				d[i][a] = d[a][i] = 1;
+			}
+		}
+
+		for (int k = 1; k <= n; k++){
+			for (int i = 1; i <= n; i++){
+				for (int j = 1; j <= n; j++){
+					if ((d[i][k] | d[k][j]) == INT_MAX)continue;
+					d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+				}
+			}
+		}
+
+		printf("Test Set #%d\n", ++tc);
+
+		int q;
+		scanf("%d", &q);
+
+		while (q--){
+			int a, b;
+			scanf("%d %d", &a, &b);
+			printf("%2d to %2d: %d\n", a, b, d[a][b]);
+		}
+
+		printf("\n"); // blank line
+	}
+}

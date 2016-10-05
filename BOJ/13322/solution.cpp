e4b3a121491e9d59c80079f36443be6a8bbb45c6
sexy_wowoto <csjaj9772@gmail.com>
2016-10-05 23:59:45 +0900

    AC with implementation

---
+++ BOJ/13322/solution.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+char str[100003];
+
+int main(){
+
+    scanf("%s", str);
+
+    int n = strlen(str);
+
+    for(int i=0; i<n; i++)printf("%d\n", i);
+
+}

6de7324273c4fe0323b54045dc4b254621e46233
sexy_wowoto <csjaj9772@gmail.com>
2016-10-08 10:26:58 +0900

    AC with implementation

---
+++ BOJ/13297/solution.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+char str[103];
+
+int main(){
+
+    int t;
+    scanf("%d", &t);
+
+    while(t--){
+
+        scanf("%s", str);
+
+        int s = strlen(str);
+
+        printf("%d\n", s);
+
+    }
+
+}

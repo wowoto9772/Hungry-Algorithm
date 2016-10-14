e001a163a59eca2a058644f2207a40c50a92b138
sexy_wowoto <csjaj9772@gmail.com>
2016-10-14 23:47:03 +0900

    AC with dp(lis)

---
+++ BOJ/11568/solution.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int lis(const vector <int> &a) {
+    vector <int> b;
+    for (auto &n : a) {
+        auto lo = lower_bound(b.begin(), b.end(), n);
+        if (lo == b.end()) {
+            b.push_back(n);
+        }
+        else {
+            *lo = n;
+        }
+    }return b.size();
+} // no duplicated 1 2 3 3 5 ==> 1 2 3 5
+
+int main(){
+
+    int n;
+    scanf("%d", &n);
+
+    vector <int> v(n);
+    for(int i=0; i<n; i++)scanf("%d", &v[i]);
+
+    printf("%d\n", lis(v));
+
+}

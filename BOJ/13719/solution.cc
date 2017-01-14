deb8659d649f9e23fe240258b4f7b6d8d6071dd7
SeungJu Choi <csjaj9772@gmail.com>
2017-01-14 17:14:37 +0900

    AC with greedy, implementation

---
+++ BOJ/13719/solution.cc
@@ -0,0 +1,33 @@
+#include <cstdio>
+
+using namespace std;
+
+int e[1000003];
+
+int main(){
+
+    int n;
+    scanf("%d", &n);
+
+    int l = 1, r = n;
+    int c = 0;
+
+    for(int i=1; i<=n; i++)scanf("%d", &e[i]);
+
+    while(l <= r){
+        if(e[l] < e[r]){
+            e[l+1] += e[l];
+            l++;
+            c++;
+        }else if(e[l] > e[r]){
+            e[r-1] += e[r];
+            r--;
+            c++;
+        }else{
+            l++, r--;
+        }
+    }
+
+    printf("%d\n", c);
+
+}

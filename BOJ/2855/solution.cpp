b039fe708a71d88d97e09fbee66b154d4fae8e08
SeungJu Choi <csjaj9772@gmail.com>
2017-11-04 21:36:36 +0900

    AC with implementation

--- BOJ/2855/solution.cpp
+++ BOJ/2855/solution.cpp
@@ -1,33 +1,44 @@
 #include <stdio.h>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int down[100003];
-int up[100003];
-int s[100003];
+#define ll long long
 
-int main() {
+ll e[100003], s[100003];
+int pr[100003], pl[100003], ans[100003];
 
-	int n, k;
-	scanf("%d %d", &n, &k);
+int main(){
+    
+    int n;
+    ll S;
+    scanf("%d %lld", &n, &S);
+    
+    for(int i=1; i<=n; i++){
+        scanf("%lld", &e[i]);
+    }
+    
+    for(int i=1; i<=n; i++)s[i] = s[i-1] + e[i];   
+    for(int i=1; i<=n; i++)pr[i] = (upper_bound(s, s+n+1, s[i-1]+S)-s) - i;
 
-	for (int i = 1; i <= n; i++) {
-		scanf("%d", &s[i]);
-		s[i] += s[i - 1];
-	}
-
-	for (int i = 1; i <= n; i++) {
-		int l = i, r = n, m;
-		while (l <= r) {
-			m = (l + r) / 2;	
-			int cmp = s[m] - s[i - 1];
-			if (cmp <= k)l = m + 1;
-			else
-				r = m - 1;
-		}
-		while (m >= i && s[m] - s[i - 1] > k)m--;
-		up[i] = m;
-	}
+    for(int i=1; i<=n; i++)s[i] = s[i-1] + e[n+1-i];
+    for(int i=1; i<=n; i++)pl[n+1-i] = (upper_bound(s, s+n+1, s[i-1]+S)-s) - i;
+    
+    for(int i=1; i<n; i++){
+        ans[i] = min(pl[i], pr[i+1]);
+        if(ans[i] > 1){
+            ans[i+1-ans[i]] = ans[i];
+            ans[i] = 0;
+        }
+    }
+    
+    int x = 0;
+    ans[n] = 0;
+    for(int i=1; i<=n; i++){
+        if(x < ans[i])x = ans[i];
+        printf("%d\n", x<<1);
+        x--;
+    }
 
 }
